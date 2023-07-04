a309294c6f53a7bc6e6dc5b148c3a044210c8fd4
Pedro <pedro.link25@gmail.com>
2023-07-03 22:21:10 -0300

    Initial commit

---
+++ src/main/java/com/hydrofuture/HydroFuture/HydroFutureApplication.java
@@ -0,0 +1,13 @@
+package com.hydrofuture.HydroFuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydroFutureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydroFutureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hydrofuture/HydroFuture/HydroFutureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hydrofuture.HydroFuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydroFutureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
